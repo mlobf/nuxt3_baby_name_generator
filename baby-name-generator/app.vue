05ca253984ed2a111301d301a49c36d100488f5c
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-23 20:31:09 -0300

    Basic App

---
+++ baby-name-generator/app.vue
@@ -0,0 +1,89 @@
+
+<script setup>
+const options = reactive({
+  gender: "Girl",
+  popularity: "Unique",
+  length: "Short",
+});
+</script> 
+
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and the find name button below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1- Choose a gender</h4>
+        <div class="option-buttons">
+          <button class="option option-left">Boy</button>
+          <button class="option option-center option-active">Girl</button>
+          <button class="option option-right">Unisex</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2- Choose popularity of the name</h4>
+        <div class="option-buttons">
+          <button class="option option-left">Unique</button>
+          <button class="option option-right">Trendy</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3- Choose name's length</h4>
+        <div class="option-buttons">
+          <button class="option option-left">Long</button>
+          <button class="option optin-center">All</button>
+          <button class="option option-right">Short</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+.container h1 {
+  font-size: 3 rem;
+}
+
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.option {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+}
+.option-left {
+  border-radius: 1rem 0 0 1rem;
+}
+.option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: 0.15rem solid rgb(249, 87, 89);
+  color: white;
+}
+</style>
